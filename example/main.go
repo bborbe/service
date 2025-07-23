109e5671ba3e22f378e09157004355e79d6a42fa
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-07-23 07:53:48 +0200

    add example service implementation

---
+++ example/main.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2023 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"os"
+
+	libhttp "github.com/bborbe/http"
+	"github.com/bborbe/run"
+	libsentry "github.com/bborbe/sentry"
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/bborbe/service"
+)
+
+func main() {
+	app := &application{}
+	os.Exit(service.Main(context.Background(), app, &app.SentryDSN, &app.SentryProxy))
+}
+
+type application struct {
+	SentryDSN   string `required:"true" arg:"sentry-dsn" env:"SENTRY_DSN" usage:"SentryDSN" display:"length"`
+	SentryProxy string `required:"false" arg:"sentry-proxy" env:"SENTRY_PROXY" usage:"Sentry Proxy"`
+	Listen      string `required:"true" arg:"listen" env:"LISTEN" usage:"address to listen to"`
+}
+
+func (a *application) Run(ctx context.Context, sentryClient libsentry.Client) error {
+	return service.Run(
+		ctx,
+		a.createMyFunc(),
+		a.createHttpServer(),
+	)
+}
+
+func (a *application) createMyFunc() run.Func {
+	return func(ctx context.Context) error {
+		// to something
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func (a *application) createHttpServer() run.Func {
+	return func(ctx context.Context) error {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		router := mux.NewRouter()
+		router.Path("/healthz").Handler(libhttp.NewPrintHandler("OK"))
+		router.Path("/readiness").Handler(libhttp.NewPrintHandler("OK"))
+		router.Path("/metrics").Handler(promhttp.Handler())
+
+		glog.V(2).Infof("starting http server listen on %s", a.Listen)
+		return libhttp.NewServer(
+			a.Listen,
+			router,
+		).Run(ctx)
+	}
+}
--- mocks/service.go
+++ mocks/service.go
@@ -88,8 +88,6 @@ func (fake *Service) RunReturnsOnCall(i int, result1 error) {
 func (fake *Service) Invocations() map[string][][]interface{} {
 	fake.invocationsMutex.RLock()
 	defer fake.invocationsMutex.RUnlock()
-	fake.runMutex.RLock()
-	defer fake.runMutex.RUnlock()
 	copiedInvocations := map[string][][]interface{}{}
 	for key, value := range fake.invocations {
 		copiedInvocations[key] = value
